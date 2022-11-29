36ba635f4b610d3f592321db595abcbc6128f456
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-11-29 12:10:50 +0200

    created defaultValue of user

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { Container } from "components/Container";
 import { Search } from "components/Search";
 import { TheHeader } from "components/TheHeader";
+import { UserCart } from "components/UserCart";
+import {defaultUser} from "mock"
+import { useState } from "react";
+import { GithubError, GithubUser, LocalGithubUser } from "types";
+import { extractLocalUser } from "utils/extract-local-user";
+import { isGithubUser } from "utils/typeguards";
+
+const BASE_URL = 'https://api.github.com/users/'
 
 function App() {
+  const [user, setUser] = useState<LocalGithubUser | null>(defaultUser)
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+    const res = await fetch(url)
+    const user = await res.json() as GithubUser | GithubError;
+    
+    if(isGithubUser(user)){
+      setUser(extractLocalUser(user));
+    }else{
+      setUser(null);
+    }
+  }
   return (
     <Container>
       <TheHeader />
-      <Search hasError onSubmit={() => {}}/>
+      <Search hasError = {!user} onSubmit={fetchUser}/>
+      {user && (
+        <UserCart {...user}/>
+      )}
     </Container>
   );
 }
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from './../types/user';
+export const defaultUser: LocalGithubUser = {
+    "login": "someVariable",
+    "id": 66574848,
+    "avatar": "https://avatars.githubusercontent.com/u/66574848?v=4",
+    "name": "Some name",
+    "company": "Som company",
+    "blog": "adada",
+    "location": "sfsfsdfsdfasd",
+    "bio": "Alternative AO's account\r\n",
+    "twitter": "adad",
+    "repos": 1,
+    "followers": 0,
+    "following": 2,
+    "created": "2020-06-07T22:18:39Z",
+    }

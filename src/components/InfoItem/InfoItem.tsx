9fa2725a25e8b5aa5093ff7abe479072da213476
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-11-29 12:07:11 +0200

    created InfoItem

---
+++ src/components/InfoItem/InfoItem.tsx
@@ -0,0 +1,33 @@
+import styles from './InfoItem.module.scss';
+
+export interface InfoItemProps {
+  icon: React.ReactNode, 
+  text?: string | null,
+  isLink?: boolean
+ }
+
+export const InfoItem = ({icon, text, isLink}: InfoItemProps) => {
+  
+  const currentText = text || "Not Available"
+  let currentHref: string = '';
+
+  if(isLink){
+    currentHref = text && text.startsWith("http") ? text : `https://${text}`
+  }
+  return(
+    <div className={`${styles.infoItem}${!text&&styles.empty}`} >
+      {icon}
+      <div className="">
+        {isLink && text ? (
+          <a 
+            href = {currentHref} 
+            target = "_blank" 
+            rel = "noreforrer" 
+            className={styles.link}>
+            {currentText}
+          </a>
+        ) : currentText}
+      </div>
+    </div>
+  );
+}
